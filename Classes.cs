7c32d9376fa80df2959f32763fe4b4cad4905e30
Your Name <you@example.com>
2022-11-18 10:54:45 +0100

    klasser

---
+++ Classes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		public class Elev() {
+		{ 
+			public int age;
+			public string name;
+			public bool height;
+			public int id;
+
+		public Car(int age, string name, bool height, int id)
+		{
+			age = age;
+			name = name;
+			height = height;
+			id = id;
+		}
+
+				public class Lärare() {
+					{ 
+						public int age;
+						public string name;
+						public bool height;
+						public int id;
+
+						public Lärare(int age, string name, bool height, int id)
+						{
+							age = age;
+							name = name;
+							height = height;
+							id = id;
+						}
+			}
+
+					public class Bil
+					{
+						public string brand;
+						public string model;
+						public int year;
+
+
+						public Bil(string carBrand, string carModel, int carYear)
+						{
+							brand = carBrand;
+							model = carModel;
+							year = carYear;
+						}
+
+
+					}
+
+
+
+
+
+
+
+
+
+
+	}
+}
